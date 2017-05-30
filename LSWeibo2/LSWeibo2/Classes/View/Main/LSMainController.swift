c6d15316973a45d686f6d8621db67dab0fc2246f
LeeMinglu <402007353@qq.com>
2017-05-31 06:58:51 +0800

    加载Main控制器并正常显示出来

--- LSWeibo2/LSWeibo2/Classes/View/Main/LSMainController.swift
+++ LSWeibo2/LSWeibo2/Classes/View/Main/LSMainController.swift
@@ -24,7 +24,39 @@ extension LSMainController {
 
     /// 设置Main控制器的子控制器
     fileprivate func setupControllers() {
-    
+        
+        let controllersDictArray = [
+            ["clsName": "LSHomeController", "title" : "首页", "imageName": "home"],
+            ["clsName": "LSDiscoverController", "title" : "发现", "imageName": "discover"],
+            ["clsName": ""],
+            ["clsName": "LSMessageController", "title" : "消息", "imageName": "message_center"],
+            ["clsName": "LSProfileController", "title" : "我", "imageName": "profile"]
+        ]
+        //这里的类型必须是UIViewController
+        var controllers = [UIViewController]()
+        
+        for dict in controllersDictArray {
+            let vc = controller(dict: dict)
+            controllers.append(vc)
+        }
+        //如果不添加此代码 main控制器中不显示
+        viewControllers = controllers
     }
     
+    fileprivate func controller(dict: [String: String]) -> UIViewController {
+        guard let clsname = dict["clsName"],
+            let title = dict["title"],
+            let imageName = dict["imageName"],
+            let clsVC = NSClassFromString(Bundle.main.namespace + "." + clsname) as? UIViewController.Type else {
+            return UIViewController()
+        }
+        
+        let vc = clsVC.init()
+        vc.title = title
+        vc.tabBarItem.image = UIImage(named: "tabbar_" + imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: "tabbar_" + imageName + "_highlighted")?.withRenderingMode(.alwaysOriginal)
+        let nav = LSNavigationController(rootViewController: vc)
+        
+        return nav
+    }
 }
