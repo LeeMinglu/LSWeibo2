080d63112de7e37fd6d0fc81841f4c5d4ecd6471
LeeMinglu <402007353@qq.com>
2017-05-30 18:48:04 +0800

    启动图片正常启动，下一步加载tabbar

---
+++ LSWeibo2/LSWeibo2/Classes/ViewModel/Tools/Extensions/UIBarButtonItem+Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  UIBarButtonItem+Extensions.swift
+//  Weibo
+//
+//  Created by Ben on 2017/4/14.
+//  Copyright © 2017年 Ben. All rights reserved.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+    
+    /// 创建 UIBarButtonItem
+    ///
+    /// - Parameters:
+    ///   - title: title
+    ///   - fontSize: fontSize,默认16号
+    ///   - target: target
+    ///   - action: action
+    ///   - isBack: 是否是返回按钮，如果是，加上箭头
+    convenience init(title: String, fontSize: CGFloat = 16, target: AnyObject?, action: Selector, isBack: Bool = false) {
+        let btn: UIButton = UIButton.cz_textButton(title, fontSize: fontSize, normalColor: UIColor.darkGray, highlightedColor: UIColor.orange)
+        btn.addTarget(target, action: action, for: .touchUpInside)
+        
+        if isBack {
+            let imageName = "navigationbar_back_withtext"
+            btn.setImage(UIImage(named: imageName), for: .normal)
+            btn.setImage(UIImage(named: imageName + "_highlighted"), for: .highlighted)
+            
+            btn.sizeToFit()
+        }
+        
+        //实例化 uibarbuttonitem
+        self.init(customView: btn)
+    }
+}
---
+++ LSWeibo2/LSWeibo2/Weibo+Bridge.h
@@ -0,0 +1,9 @@
+//
+//  Weibo+Bridge.h
+//  LSWeibo2
+//
+//  Created by 李明禄 on 2017/5/30.
+//  Copyright © 2017年 SocererGroup. All rights reserved.
+//
+
+#import "CZAdditions.h"
